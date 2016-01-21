35b44687647576933cbd1b15fcff3d51f08b9e22
David Olivari <david.olivari@gmail.com>
2016-01-21 12:15:05 +0100

    Initial commit

---
+++ tools/osc_universal_server.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+
+from liblo import *
+import sys
+
+class MyServer(ServerThread):
+    def __init__(self):
+        ServerThread.__init__(self, 8000)
+
+    @make_method(None, None)
+    def fallback(self, path, args):
+        print "received message '%s'" % path
+        print args
+try:
+    server = MyServer()
+except ServerError, err:
+    print str(err)
+    sys.exit()
+
+server.start()
+raw_input("press enter to quit...\n")
