d6ad674e797c4e54cf15591820ade48a41025511
David Olivari <david.olivari@gmail.com>
2016-02-05 11:17:54 +0100

    Xmas tree demo

--- tools/osc_neopixel_test.py
+++ tools/osc_neopixel_test.py
@@ -1,11 +1,21 @@
+#!/usr/bin/env python2
+# coding=utf8
 
+from time import time, sleep
+from random import randint
+from liblo import *
+import colorsys
+target = Address("169.254.1.1", 9000)
 
-e = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+l = 9
+e = [0] * (9 * 3)
+
 while True:
-	for i in range(15):
-		e[i]=random.randint(0,255)
-	send(target,"/outputs/rgb/16",e)
-	time.sleep(0.01)
-	e = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	send(target,"/outputs/rgb/16",e)
-	time.sleep(0.1)
+    for i in range(l):
+    	c =  colorsys.hsv_to_rgb(randint(0,100)/100.0,1,1)
+        e[i * 3] = int(c[0]*128)
+        e[i * 3 + 1] = int(c[1]*200)
+        e[i * 3 + 2] = int(c[2]*255)
+
+    send(target, "/outputs/rgb/16", e)
+    sleep(0.05)
