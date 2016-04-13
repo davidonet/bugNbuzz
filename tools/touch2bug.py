97e46469246edebb03712b24cd1973e2ce48ba45
cie concordance <cieconcordance@MacBook-Pro-de-cie.local>
2016-04-13 15:03:31 +0200

    after Kalaj

--- tools/touch2bug.py
+++ tools/touch2bug.py
@@ -1,23 +1,52 @@
 #!/usr/bin/env python2
+#from subprocess import Popen, PIPE
+
 
 def touch2bug(dst, path, args):
 	if dst == "ardour":
 		splited = path.split("/")
 		ard_path = "/" + splited[1] + "/" + splited[2] + "/" + splited[3]
+		
+
 		multiple = int(splited[4]) - 1
-		if splited[3] == "gainabs":
-			track = multiple * 8 + int(splited[5]) + multiple
-			arg = float(args[0])
-		if splited[3] == "mute":
-			track = multiple * 8 + int(splited[6]) + multiple
-			arg = int(args[0])
+		if multiple == 317:
+			if splited[3] == "gainabs":
+				track = 318
+				arg = float(args[0])
+		else:
+			if splited[3] == "gainabs":
+				track = multiple * 8 + int(splited[5]) + multiple
+				arg = float(args[0])
+			elif splited[3] == "mute":
+				track = multiple * 8 + int(splited[6]) + multiple
+				arg = int(args[0])
 		return ard_path, track, arg
+		
+#	 	Adrour      REGISTER_CALLBACK (serv, "/ardour/routes/plugin/parameter", "iiif", route_plugin_parameter);
+#					params are Route-ID Plugin-Number Parameter-Number Value */
+#		Touchosc	/ardour/route/plugin/parameter/[tracknumer]/[Plugin-number]/[Parameter-number] [arg]
+		if splited[3] == "plugin":
+			ard_path = ard_path + splited[4]
+			track = int(splited[5])
+			plugin = int(splited[6])
+			parm = int(splited[7])
+			args = plugin, parm, float(arg[0])
+		return ard_path, track, args
 		
 	if dst =="carla":
 		splited = path.split("/")
 		carla_path = "/" + splited[1] + "/" + str(int(splited[4]) - 1) + "/" + splited[2]
 		arg = int(args[0])
 		return carla_path, arg
+		
+	
+#	if dst =="mplayer":
+#		splited = path.split("/")
+#		if splited[1] = "play":
+#			tracknbr = int(args[1])
+#			...
+#		if splited[1] = "stop":
+#			...
 
 
 if __name__ == '__main__':
