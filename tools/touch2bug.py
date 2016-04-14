fb2cdae73daa9abbc40e3965e2812d9e68a71fc5
cie concordance <cieconcordance@MacBook-Pro-de-cie.local>
2016-04-14 16:48:35 +0200

    refactor ardour's osc handling

--- tools/touch2bug.py
+++ tools/touch2bug.py
@@ -12,29 +12,29 @@ def touch2bug(dst, path, args):
 		
 
 		multiple = int(splited[4]) - 1
-		if multiple == 317:
-			if splited[3] == "gainabs":
-				track = 318
-				arg = float(args[0])
-		else:
-			if splited[3] == "gainabs":
+		
+		if splited[3] == "gainabs":
+			if multiple == 317:
+				track == 318
+			else:
 				track = multiple * 8 + int(splited[5]) + multiple
-				arg = float(args[0])
-			elif splited[3] == "mute":
-				track = multiple * 8 + int(splited[6]) + multiple
-				arg = int(args[0])
-		return ard_path, track, arg
+			arg = float(args[0])
+		
+		elif splited[3] == "mute":
+			track = multiple * 8 + int(splited[6]) + multiple
+			arg = int(args[0])
 		
 #	 	Adrour      REGISTER_CALLBACK (serv, "/ardour/routes/plugin/parameter", "iiif", route_plugin_parameter);
 #					params are Route-ID Plugin-Number Parameter-Number Value */
 #		Touchosc	/ardour/route/plugin/parameter/[tracknumer]/[Plugin-number]/[Parameter-number] [arg]
-		if splited[3] == "plugin":
+		elif splited[3] == "plugin":
 			ard_path = ard_path + splited[4]
 			track = int(splited[5])
 			plugin = int(splited[6])
 			parm = int(splited[7])
-			args = plugin, parm, float(arg[0])
-		return ard_path, track, args
+			arg = plugin, parm, float(arg[0])
+		
+		return ard_path, track, arg
 		
 	if dst =="carla":
 		splited = path.split("/")
