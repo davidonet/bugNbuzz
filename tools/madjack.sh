7565005c8d6f31c0976ed46212f9ce0b80450d97
Wargreen <wargreen@lebib.org>
2016-05-26 16:04:26 +0200

    clean session

--- tools/madjack.sh
+++ tools/madjack.sh
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 #!/bin/sh
 sleep 10s
 ~/bugNbuzz/tools/madjack -p 10000 -d ~/bugNbuzz/mp3 -l "ardour:mp3/audio_in 1" -r "ardour:mp3/audio_in 2"
-=======
-#!/bin/sh 
-~/bugNbuzz/tools/madjack -p 10000 -d ~/bugNbuzz/mp3
->>>>>>> d1828ce587c5f1e3d8ee2a8d8f49b8a99fdb19c6
