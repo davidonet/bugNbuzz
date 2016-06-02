dacc952f1f8d365ba3f8058a2d2e6707b2c708ae
cieconcordance <cieconcordance@infonik.kalaj.lan>
2016-06-02 18:40:16 +0200

    xterm for madjack

--- tools/madjack.sh
+++ tools/madjack.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sleep 10s
-~/bugNbuzz/tools/madjack -p 10000 -d ~/bugNbuzz/mp3 -l "ardour:mp3/audio_in 1" -r "ardour:mp3/audio_in 2"
+
+#sleep 10s
+
+xterm -e ~/bugNbuzz/tools/madjack -p 10000 -d ~/bugNbuzz/mp3 -l "ardour:mp3/audio_in 1" -r "ardour:mp3/audio_in 2"
